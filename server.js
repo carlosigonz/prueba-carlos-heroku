2c61f443511484a0392d84d19abae572b45122cd
Carlos Gonzalez <carlosigonz@gmail.com>
2022-08-01 20:57:56 -0400

    mensaje

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res
         .status(200)
-        .send('Hello server is running')
+        .send('Hola Jhoel todo bien con la prueba')
         .end();
 });
 
